c0e36979f858356210abf03ae19a4a2d3c05bf7c
Atharv Tale <atharvtale2002@gmail.com>
2023-05-18 18:22:14 +0530

    s

--- index.js
+++ index.js
@@ -61,23 +61,44 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 // ... existing code ...
-app.use('/user', userRoutes)
-app.use('/questions', questionsRoutes)
-app.use('/answer', answerRoutes)
+app.use('/user', userRoutes);
+app.use('/questions', questionsRoutes);
+app.use('/answer', answerRoutes);
 
 const PORT = 9999;
 const CONNECT_URL = process.env.CONNECTION_URL;
 
-mongoose.connect(CONNECT_URL, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true
+app.get('/', (req, res) => {
+    res.send("This is a stack overflow clone API")
 })
-  .then(() => {
-    console.log('Connected to the database');
-    app.listen(PORT, () => {
-      console.log(`Server running on port ${PORT}`);
-    });
-  })
-  .catch((error) => {
-    console.error(error.message);
-  });
+
+// mongoose.connect(CONNECT_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true
+// })
+//   .then(() => {
+//     console.log('Connected to the database');
+//     app.listen(PORT, () => {
+//       console.log(`Server running on port ${PORT}`);
+//     });
+//   })
+//   .catch((error) => {
+//     console.error(error.message);
+//   });
+
+  const connectToDatabase = async () => {
+    try {
+       mongoose.createConnection(CONNECT_URL, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true
+      });
+      console.log(`Connected to the database`);
+      app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
+    } catch (error) {
+      console.error(error.message);
+    }
+  };
+  
+  connectToDatabase();
